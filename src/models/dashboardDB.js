67ceac9f41880bfa51a7d0c4c195808a47c9df40
AngelSvX <lsangelenma21@gmail.com>
2025-06-09 23:25:08 -0500

    Corrección

--- src/models/dashboardDB.js
+++ src/models/dashboardDB.js
@@ -7,7 +7,8 @@ export const dashboardDB = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE
+  database: process.env.DB_DATABASE,
+  port: process.env.DB_PORT
 })
 
 // Verificar la conexión de la DB
